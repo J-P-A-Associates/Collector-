3818a22b050bd31a685cc770baf14ea41dca6552
J-P-A-Associates <austin189@gmail.com>
2025-12-03 14:53:05 -0500

    Create page.tsx

---
+++ src/app/collections/page.tsx
@@ -0,0 +1,8 @@
+export default function Collections() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold mb-8">My Collections</h1>
+      <p className="text-lg">Your collections will appear here once you create them!</p>
+    </div>
+  )
+}
